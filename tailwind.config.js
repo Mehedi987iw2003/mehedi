ba8c5cf6f3e401aefaa1820d36f9b8c0a84197e0
Mehedi987iw2003 <mh649150@gmail.com>
2025-07-19 20:54:22 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*",
+  ],
+  safelist: ['hidden', 'block', 'sm:block'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
